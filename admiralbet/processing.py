88ca719659375bbbce702579752689750b88019f
PokarPetr <pokar.petr@gmail.com>
2024-09-11 11:31:29 +0200

    corrected names in processing.py

--- admiralbet/processing.py
+++ admiralbet/processing.py
@@ -25,15 +25,21 @@ def determine_type_name(name, mode):
             '1154': 1,
             '1155': 2
         }
+        time_map = {
+            '1': 'Home',
+            '2': "Away",
+        }
         winner = winner_map.get(mode, '1')
+        team_match = re.search(r'Team(\d)', name)
+        team_number = team_match.group(1) if team_match else None
         patterns = [
-            {"pattern": "Team", "type_name": f"{winner} Wins and Team Total Home"},
-            {"pattern": "not", "type_name": f"Not {winner} Wins and Total Goals"},
-            {"pattern": "GGII", "type_name": f"{winner} Wins, Second Time Both Teams to Goal"},
-            {"pattern": "GGI", "type_name": f"{winner} Wins, First Time Both Teams to Goal"},
-            {"pattern": "GG", "type_name": f"{winner} Wins, Both Teams to Goal"},
-            {"pattern": "&", "type_name": f"{winner} Wins and Goals"},
-        ]
+                {"pattern": "Team", "type_name": f"{winner} Wins and {time_map.get(team_number, '')} Total Goals"},
+                {"pattern": "not", "type_name": f"Not {winner} Wins and Total Goals"},
+                {"pattern": "GGII", "type_name": f"{winner} Wins, Second Time Both Teams to Goal"},
+                {"pattern": "GGI", "type_name": f"{winner} Wins, First Time Both Teams to Goal"},
+                {"pattern": "GG", "type_name": f"{winner} Wins, Both Teams to Goal"},
+                {"pattern": "&", "type_name": f"{winner} Wins and Goals"},
+            ]
 
     for pattern in patterns:
         if pattern['pattern'] in name:
@@ -55,27 +61,24 @@ async def reformat_bets(bets):
         139: {"type_name": lambda bet: f"1H{bet.get('type_name', '')}"},
         140: {"type_name": "Win First Time - Win Match"},
         141: {"type_name": "First Time Team Total Home",
-              "type_value": lambda name: "O" if name == "Over" else "U"},
+              "type_value": lambda name: "1HHTO" if name == "Over" else "1HHTU"},
         142: {"type_name": "First Time Team Total Away",
-              "type_value": lambda name: "O" if name == "Over" else "U"},
-        143: {"type_name": "First Time Total Goals", "type_value": lambda name: "O" if name == "Over" else "U"},
+              "type_value": lambda name: "1HATO" if name == "Over" else "1HATU"},
+        143: {"type_name": "First Time Total Goals", "type_value": lambda name: "1HO" if name == "Over" else "1HU"},
         161: {"type_name": "Second Time Team Total Home",
-              "type_value": lambda name: "O" if name == "Over" else "U"},
+              "type_value": lambda name: "2HHTO" if name == "Over" else "2HHTU"},
         162: {"type_name": "Second Time Team Total Away",
-              "type_value": lambda name: "O" if name == "Over" else "U"},
-        163: {"type_name": "Second Time Total Goals", "type_value": lambda name: "O" if name == "Over" else "U"},
-        # 1154: {"type_name": lambda name: determine_type_name(name, '1154')},
-        # 1155: {"type_name": lambda name: determine_type_name(name, '1155')},
-        # 1380: {"type_name": lambda name: determine_type_name(name, '1380')},
+              "type_value": lambda name: "2HATO" if name == "Over" else "2HATU"},
+        163: {"type_name": "Second Time Total Goals", "type_value": lambda name: "2HO" if name == "Over" else "2HU"},
         454: {"type_name": "1X2"},
-        221: {"type_name": "Set Handicap", "type_value": lambda name: "AH1" if name == "1" else "AH2"},
-        220: {"type_name": "Game Handicap", "type_value": lambda name: "AH1" if name == "1" else "AH2"},
+        221: {"type_name": "Set Asian Handicap", "type_value": lambda name: "AH1" if name == "1" else "AH2"},
+        220: {"type_name": "Game Asian Handicap", "type_value": lambda name: "GAH1" if name == "1" else "GAH2"},
         187: {"type_name": "Total Games", "type_value": lambda name: "O" if name == "More" else "U"},
-        215: {"type_name": "1 Set Total Games", "type_value": lambda name: "O" if name == "Over" else "U"},
-        218: {"type_name": "1 Set Total Games", "type_value": lambda name: "O" if name == "Over" else "U"},
-        219: {"type_name": "2 Set Total Games", "type_value": lambda name: "O" if name == "Over" else "U"},
-        229: {"type_name": "Set Win", "type_value": lambda name: "Yes" if name == "Yes" else "No"},
-        230: {"type_name": "Set Win", "type_value": lambda name: "Yes" if name == "Yes" else "No"}
+        215: {"type_name": "1 Set Total Games", "type_value": lambda name: "1HO" if name == "Over" else "1HU"},
+        218: {"type_name": "1 Set Total Games", "type_value": lambda name: "1HO" if name == "Over" else "1HU"},
+        219: {"type_name": "2 Set Total Games", "type_value": lambda name: "2HO" if name == "Over" else "2HU"},
+        229: {"type_name": "First Player Wins Set", "type_value": lambda name: "Yes" if name == "Yes" else "No"},
+        230: {"type_name": "Second Player Wins Set", "type_value": lambda name: "Yes" if name == "Yes" else "No"}
     }
 
     for bet in bets:
